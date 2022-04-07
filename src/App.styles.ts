10a723a4d73e20cb6832d7e65ddb207fc4d2ba1d
Rodrigo Orozco <orozcorodi8@gmail.com>
2022-04-07 19:39:55 -0300

    To Do List Uploaded

---
+++ src/App.styles.ts
@@ -0,0 +1,106 @@
+import styled from "styled-components";
+
+export const H1 = styled.h1`
+  text-align: center;
+  text-transform: uppercase;
+  font-weight: bold;
+  color: whitesmoke;
+`;
+
+export const Conteiner = styled.div`
+  background-image: radial-gradient(
+    circle at 83.64% 78.23%,
+    #94f1ff 0,
+    #76abff 25%,
+    #536ef2 50%,
+    #0300ad 75%,
+    #003871 100%
+  );
+  display: flex;
+  justify-content: center;
+  min-height: 100vh;
+`;
+export const Card = styled.div`
+  background-color: rgba(255, 255, 255, 0.124);
+  border-radius: 20px;
+  width: 30%;
+  height: 60%;
+  padding: 10px;
+  margin-top: 5%;
+  margin-bottom: 5%;
+`;
+
+export const Search = styled.div`
+  display: flex;
+  margin: 0.5em;
+  margin-bottom: 0.8em;
+  flex-direction: row;
+  justify-content: center;
+  width: auto;
+  height: 30px;
+  border-radius: 15px 15px 15px 15px;
+
+  input {
+    font-size: 20px;
+    width: 75%;
+    height: 100%;
+    border-radius: 15px 0px 0px 15px;
+  }
+  button {
+    font-size: 20px;
+    border-radius: 0px 15px 15px 0px;
+    width: 25%;
+    height: 120%;
+    cursor: pointer;
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  border-radius: 15px 15px 15px 15px;
+`;
+export const Tasks = styled.div`
+  margin: 0.5em;
+  border-radius: 5px;
+  background-color: rgba(255, 255, 255, 0.904);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  width: 94%;
+  height: 13%;
+  justify-content: space-between;
+`;
+
+export const H2 = styled.h2`
+  padding-left: 5%;
+  overflow: auto;
+  width: 77%;
+  height: 100%;
+`;
+
+export const Buttons = styled.div`
+  height: 100%;
+  width: 15%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Hacer = styled.button`
+  height: 45%;
+  width: 95%;
+  background-color: rgba(157, 28, 255, 0.968);
+  color: white;
+`;
+
+export const Papelera = styled.button`
+  background-color: red;
+  color: white;
+  height: 45%;
+  width: 95%;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import {
+  H1,
+  H2,
+  Conteiner,
+  Card,
+  Search,
+  Form,
+  Tasks,
+  Buttons,
+  Papelera,
+  Hacer,
+} from "./App.styles";
 
-function App() {
+type FormElement = React.FormEvent<HTMLFormElement>;
+interface ITask {
+  name: string;
+  done: boolean;
+}
+
+function App(): JSX.Element {
+  const [newTask, setNewTask] = useState<string>("");
+  const [tasks, setTasks] = useState<ITask[]>([]);
+  const taskInput = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: FormElement) => {
+    if (newTask.length) {
+      e.preventDefault();
+      addTask(newTask);
+      setNewTask("");
+      taskInput.current?.focus();
+    } else {
+      e.preventDefault();
+      alert("Debes agregar una tarea!");
+      setNewTask("");
+      taskInput.current?.focus();
+    }
+  };
+
+  const addTask = (name: string): void => {
+    const newTasks: ITask[] = [...tasks, { name, done: false }];
+    setTasks(newTasks);
+  };
+
+  const toggleDoneTask = (i: number): void => {
+    const newTasks: ITask[] = [...tasks];
+    newTasks[i].done = !newTasks[i].done;
+    setTasks(newTasks);
+  };
+
+  const removeTask = (i: number): void => {
+    const newTasks: ITask[] = [...tasks];
+    newTasks.splice(i, 1);
+    setTasks(newTasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Conteiner>
+      <Card>
+        <H1>To Do List</H1>
+        <Search>
+          <Form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder=" Agregar tarea..."
+              onChange={(e) => setNewTask(e.target.value)}
+              value={newTask}
+              autoFocus
+              ref={taskInput}
+            />
+            <button>Insertar</button>
+          </Form>
+        </Search>
+        {tasks?.map((task: ITask, i: number) => (
+          <Tasks key={i}>
+            <H2
+              className="H2"
+              style={{ textDecoration: task.done ? "line-through" : "" }}
+            >
+              {task.name}
+            </H2>
+            <Buttons>
+              <Hacer
+                onClick={() => toggleDoneTask(i)}
+                style={{
+                  backgroundColor: task.done
+                    ? "green"
+                    : "rgba(157, 28, 255, 0.968)",
+                }}
+              >
+                {task.done ? "✓" : "✗"}
+              </Hacer>
+              <Papelera onClick={() => removeTask(i)}>🗑</Papelera>
+            </Buttons>
+          </Tasks>
+        ))}
+      </Card>
+    </Conteiner>
   );
 }
 
